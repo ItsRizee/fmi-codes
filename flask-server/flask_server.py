2783056664c756002fea06795b21c14605f48bdc
MontBru <monticelli.bryan@gmail.com>
2025-03-16 14:32:51 +0200

    fixed flask server

--- flask-server/flask_server.py
+++ flask-server/flask_server.py
@@ -6,9 +6,11 @@ from orekit.pyhelpers import setup_orekit_curdir, download_orekit_data_curdir
 from datetime import timedelta, datetime
 from org.orekit.time import AbsoluteDate, TimeScalesFactory
 from org.orekit.propagation.analytical.tle import TLE, TLEPropagator
+from org.orekit.utils import Constants as orekit_constants
 from math import radians, degrees
 import time
 import inspect
+from orekit.pyhelpers import datetime_to_absolutedate
 
 # Global Variables for JVM and Time Scale
 vm = None
@@ -46,42 +48,109 @@ def possibility_of_collision(s, d):
 
     date = convert_day_of_year(s["EpochYear"], s["EpochDay"])
 
-    print(dir(TLE))
-    # constructors = [name for name, method in inspect.getmembers(TLE, predicate=inspect.isfunction) if name=="__init__"]
-    # print(constructors)
+    # a = 7000.0e3  # meters
+    # e = 0.001
+    # i = float(np.deg2rad(98.0))  # Conversion to Python float is required for Orekit
+    # pa = float(np.deg2rad(42.0))
+    # raan = float(np.deg2rad(42.0))
+    # ma = float(np.deg2rad(42.0))  # Mean anomaly
+    # satellite_number = 99999
+    # classification = 'X'
+    # launch_year = 2018
+    # launch_number = 42
+    # launch_piece = 'F'
+    # ephemeris_type = 0
+    # element_number = 999
+    # revolution_number = 100
+    # date_start = datetime(2019, 1, 1)
 
+    e = float(s["Eccentricity"])
+    i = float(s["Inclination"])  # Conversion to Python float is required for Orekit
+    pa = float(s["ArgumentOfPerigee"])
+    raan = float(s["RightAscensionOfAscendingNode"])
+    ma = float(s["MeanAnomaly"])  # Mean anomaly
+    satellite_number = int(s["SatelliteNumber"])
+    classification = str(s["Classification"])
+    launch_year = int(launchY)
+    launch_number = int(launchN)
+    launch_piece = str(launch_piece)
+    ephemeris_type = 0
+    element_number = 999
+    revolution_number = int(s["RevolutionNumberAtEpoch"])
+    date_start = datetime(date.year, date.month, date.day)
 
-    line1_1 = "1 25544U 98067A   24001.00000000  .00000000  00000-0  00000-0 0  9999"
-    line2_1 = "2 25544  51.6400  97.4300 0005700 182.0200 178.6500 15.50000000000000"
+    date_start_orekit = datetime_to_absolutedate(date_start)
+    mean_motion = float(s["MeanMotion"])
+    mean_motion_first_derivative = float(s["FirstTimeDerivativeOfMeanMotion"])
+    mean_motion_second_derivative = float(s["SecondTimeDerivativeOfMeanMotion"])
+    b_star_first_guess = float(s["BstarDragTerm"])  # Does not play any role, because it is a free parameter when fitting the TLE
 
-    line1_2 = "1 44888U 24001A   24001.00000000  .00000000  00000-0  00000-0 0  9999"
-    line2_2 = "2 44888  51.6401  97.4301 0005701 182.0201 178.6501 15.50000000000000"
-    tle1 = TLE(line1_1, line2_1)
-    tle2 = TLE(line1_2, line2_2)
-
-    # tle1 = TLE(
-    #     s["SatelliteNumber"], s["Classification"], launchY, launchN, launchP, 0, 0,
-    #     AbsoluteDate(date.year, date.month, date.day, utc),
-    #     s["MeanMotion"], s["FirstTimeDerivativeOfMeanMotion"], s["SecondTimeDerivativeOfMeanMotion"],
-    #     s["Eccentricity"], s["Inclination"], s["ArgumentOfPerigee"],
-    #     s["RightAscensionOfAscendingNode"], s["MeanAnomaly"],
-    #     s["RevolutionNumberAtEpoch"], s["BstarDragTerm"]
-    # )
+    tle1 = TLE(satellite_number, 
+                            classification,
+                            launch_year,
+                            launch_number,
+                            launch_piece,
+                            ephemeris_type,
+                            element_number,
+                            date_start_orekit,
+                            mean_motion,
+                            mean_motion_first_derivative, 
+                            mean_motion_second_derivative,
+                            e,
+                            i,
+                            pa,
+                            raan,
+                            ma,
+                            revolution_number,
+                            b_star_first_guess)
 
     launchY = 1900 + int(d["InternationalDesignator"][:2])
     launchN = d["InternationalDesignator"][2:5]
     launchP = d["InternationalDesignator"][5:8]
 
+    utc = TimeScalesFactory.getUTC()
+
     date = convert_day_of_year(d["EpochYear"], d["EpochDay"])
 
-    # tle2 = TLE(
-    #     d["SatelliteNumber"], d["Classification"], launchY, launchN, launchP, 0, 0,
-    #     AbsoluteDate(date.year, date.month, date.day, utc),
-    #     d["MeanMotion"], d["FirstTimeDerivativeOfMeanMotion"], d["SecondTimeDerivativeOfMeanMotion"],
-    #     d["Eccentricity"], d["Inclination"], d["ArgumentOfPerigee"],
-    #     d["RightAscensionOfAscendingNode"], d["MeanAnomaly"],
-    #     d["RevolutionNumberAtEpoch"], d["BstarDragTerm"]
-    # )
+    e = float(d["Eccentricity"])
+    i = float(d["Inclination"])  # Conversion to Python float is required for Orekit
+    pa = float(d["ArgumentOfPerigee"])
+    raan = float(d["RightAscensionOfAscendingNode"])
+    ma = float(d["MeanAnomaly"])  # Mean anomaly
+    satellite_number = int(d["SatelliteNumber"])
+    classification = str(d["Classification"])
+    launch_year = int(launchY)
+    launch_number = int(launchN)
+    launch_piece = str(launch_piece)
+    ephemeris_type = 0
+    element_number = 999
+    revolution_number = int(d["RevolutionNumberAtEpoch"])
+    date_start = datetime(date.year, date.month, date.day)
+
+    date_start_orekit = datetime_to_absolutedate(date_start)
+    mean_motion = float(d["MeanMotion"])
+    mean_motion_first_derivative = float(d["FirstTimeDerivativeOfMeanMotion"])
+    mean_motion_second_derivative = float(d["SecondTimeDerivativeOfMeanMotion"])
+    b_star_first_guess = float(d["BstarDragTerm"])  # Does not play any role, because it is a free parameter when fitting the TLE
+
+    tle2 = TLE(satellite_number, 
+                            classification,
+                            launch_year,
+                            launch_number,
+                            launch_piece,
+                            ephemeris_type,
+                            element_number,
+                            date_start_orekit,
+                            mean_motion,
+                            mean_motion_first_derivative, 
+                            mean_motion_second_derivative,
+                            e,
+                            i,
+                            pa,
+                            raan,
+                            ma,
+                            revolution_number,
+                            b_star_first_guess)
 
     # Create TLE propagators
     propagator1 = TLEPropagator.selectExtrapolator(tle1)
